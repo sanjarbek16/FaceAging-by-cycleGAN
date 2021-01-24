370ec63b93a4955fe44d09f7a00b198490048e17
sanjarbek16 <sanjarbek@kaist.ac.kr>
2021-01-24 23:36:43 +0900

    training data for colab

--- datasets/selfies/crop.py
+++ datasets/selfies/crop.py
@@ -19,21 +19,21 @@ def crop_center(pil_img, crop_width, crop_height):
 def crop_max_square(pil_img):
     return crop_center(pil_img, min(pil_img.size), min(pil_img.size))
 
-for i in range(11):
+for i in range(1, 2003):
 
 
-    my_file = Path('testB/selfie'+str(i)+'.jpg')
-    my_file2 = Path('testB/selfie'+str(i)+'.jpeg')
+    my_file = Path('new/face ('+str(i)+').jpg')
+    my_file2 = Path('new/face ('+str(i)+').jpeg')
     if my_file.is_file():
     # file exists
-        im = Image.open('testB/selfie'+str(i)+'.jpg')
+        im = Image.open('new/face ('+str(i)+').jpg')
         im_thumb = crop_max_square(im).resize((thumb_width, thumb_width), Image.LANCZOS)
-        im_thumb.save('testB/selfie'+str(i)+'.jpg', quality=95)
+        im_thumb.save('new/face ('+str(i)+').jpg', quality=95)
     elif my_file2.is_file():
-        im = Image.open('testB/selfie'+str(i)+'.jpeg')
+        im = Image.open('new/face ('+str(i)+').jpeg')
         im_thumb = crop_max_square(im).resize((thumb_width, thumb_width), Image.LANCZOS)
-        im_thumb.save('testB/selfie'+str(i)+'.jpg', quality=95)
+        im_thumb.save('new/face ('+str(i)+').jpg', quality=95)
     else:
-        im = Image.open('testB/selfie'+str(i)+'.png')
+        im = Image.open('new/face ('+str(i)+').png')
         im_thumb = crop_max_square(im).resize((thumb_width, thumb_width), Image.LANCZOS)
-        im_thumb.save('testB/selfie'+str(i)+'.png', quality=95)
+        im_thumb.save('new/face ('+str(i)+').png', quality=95)
